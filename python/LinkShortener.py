f7af246c64e5302ba76b52783ba3c6af64a0d96f
Angaar <48874687+Rohit-554@users.noreply.github.com>
2021-10-27 10:03:54 +0530

    Delete LinkShortener.py

--- python/LinkShortener.py
+++
@@ -1,5 +0,0 @@
-import pyshorteners                                                                       #import pyshorteners CMD "pip install pyshorteners"
-url = input("Enter The Url : ")
-shortener = pyshorteners.Shortener()
-x=shortener.tinyurl.short(url)
-print(x)                                                                          
